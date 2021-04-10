1127aa0bc4d8e0d83de618de7dad8b0778b54832
tatu_heinamaki <tatu.heinamaki@gmail.com>
2021-04-11 00:36:51 +0300

    Created folders and scenes for main menu and gameplay.  Added TextMeshPro samples to project.

---
+++ Assets/Scripts/PauseControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Handles pausing the game. Esc brings up the main menu
+public class PauseControl : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public static bool gameIsPaused;
+
+    void Start()
+    {
+        if(mainMenu == null)
+        {
+            mainMenu = GameObject.FindGameObjectWithTag("Pause Menu");
+        }
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            gameIsPaused = !gameIsPaused;
+            PauseGame();
+        }
+    }
+
+    void PauseGame ()
+    {
+        if(gameIsPaused)
+        {
+            mainMenu.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else 
+        {
+            mainMenu.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadSceneAsync("Gameplay", LoadSceneMode.Additive);
+        SceneManager.UnloadSceneAsync("MainMenu");
+    }
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadSceneAsync("MainMenu", LoadSceneMode.Additive);
+        SceneManager.UnloadSceneAsync("Gameplay");
+        
+    }
+}
