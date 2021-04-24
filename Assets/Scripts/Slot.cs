4355c4a085f7755075cfa189d99b46296d24b773
Petteri Heinonen <petteri.heinonen@seclion.fi>
2021-04-24 22:17:49 +0300

    Init slots from prefabs

---
+++ Assets/Scripts/Slot.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Slot : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData pointerEventData) {
+        if (pointerEventData != null) {
+            pointerEventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+        }
+    }
+}
