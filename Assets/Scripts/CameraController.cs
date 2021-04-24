148e90794a04d3d76b114d4704e55794999b7ed4
tatu_heinamaki <tatu.heinamaki@gmail.com>
2021-04-24 13:28:24 +0300

    Changed agentsprite animation in CameraMovement.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -11,16 +11,20 @@ public class CameraController : MonoBehaviour
     [SerializeField] private float itemsPos;
     //[SerializeField] private float reportsPos;
     [SerializeField] private float cameraSpeed;
-    [SerializeField] private Coroutine thisCoroutine;
+    [SerializeField] private Coroutine cameraCoroutine;
     public bool canMove = true;
     [Space(10)]
     // Agent sprite stuff
     [SerializeField] private GameObject agentSprite;
+    [SerializeField] private float agentCaseFilePos;
+    [SerializeField] private float agentItemsPos;
     private float agentPosY;
     private bool flipX;
+    [SerializeField] private Coroutine agentCoroutine;
     void Start()
     {
        cam = Camera.main;
+
        agentPosY = agentSprite.transform.position.y;
        flipX = agentSprite.GetComponent<SpriteRenderer>().flipX;
 
@@ -47,14 +51,21 @@ public class CameraController : MonoBehaviour
     public void MoveToCaseFile()
     {
         StopAllCoroutines();
-        thisCoroutine = null;
-        thisCoroutine = StartCoroutine(MoveCamera(cam.transform.position.x, caseFilePos));
+        cameraCoroutine = null;
+        cameraCoroutine = StartCoroutine(MoveCamera(cam.transform.position.x, caseFilePos));
+
+        agentCoroutine = null;
+        agentCoroutine = StartCoroutine(MoveAgent(agentSprite.transform.position.x, agentCaseFilePos));
     }
+
     public void MoveToItems()
     {
         StopAllCoroutines();
-        thisCoroutine = null;
-        thisCoroutine = StartCoroutine(MoveCamera(cam.transform.position.x, itemsPos));
+        cameraCoroutine = null;
+        cameraCoroutine = StartCoroutine(MoveCamera(cam.transform.position.x, itemsPos));
+
+        agentCoroutine = null;
+        agentCoroutine = StartCoroutine(MoveAgent(agentSprite.transform.position.x, agentItemsPos));
     }
     /*public void MoveToReports()
     {
@@ -71,17 +82,29 @@ public class CameraController : MonoBehaviour
         {
             newPos = Mathf.Lerp(startPos, endPos, timeElapsed/cameraSpeed);
             cam.transform.position = new Vector3(newPos,0,0);
-            agentSprite.transform.position = new Vector3(newPos,agentPosY,0);
             timeElapsed += Time.deltaTime;
             yield return null;
         }
         newPos = endPos;
         cam.transform.position = new Vector3(newPos,0,0);
-
+        yield return null;
+    }
+    IEnumerator MoveAgent(float startPos, float endPos)
+    {
+        float timeElapsed = 0;
+        float newPos = 0;
+        while(timeElapsed < cameraSpeed)
+        {
+            newPos = Mathf.Lerp(startPos, endPos, timeElapsed/cameraSpeed);
+            agentSprite.transform.position = new Vector3(newPos,agentPosY,0);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+        newPos = endPos;
         agentSprite.transform.position = new Vector3(newPos,agentPosY,0);
         agentSprite.GetComponent<SpriteRenderer>().flipX = !flipX;
         flipX = agentSprite.GetComponent<SpriteRenderer>().flipX;
-        
+
         yield return null;
     }
 }
