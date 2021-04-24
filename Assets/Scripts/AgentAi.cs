cbef6b861894ec77d098defb094fe8b6a58cb81f
tatu_heinamaki <tatu.heinamaki@gmail.com>
2021-04-24 21:50:17 +0300

    Created animation for agent when they enter the scene.

---
+++ Assets/Scripts/AgentAi.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentAi : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private Coroutine agentColorCoroutine;
+    [SerializeField] private Coroutine agentPosCoroutine;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] float agentSpeed = 2f;
+    [SerializeField] TypeWriterEffect typeWriterEffect;
+    void Start()
+    {
+        if(animator == null)
+        {
+            animator = GetComponent<Animator>();
+        }
+        if(spriteRenderer == null)
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+        AgentEnters();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void AgentEnters()
+    {
+        animator.SetBool("isWalking", true);
+        spriteRenderer.color = new Color32(255,255,255,0);
+
+        agentColorCoroutine = StartCoroutine(AgentColor());
+        agentPosCoroutine = StartCoroutine(AgentPos());
+    }
+    IEnumerator AgentColor()
+    {
+        float timeElapsed = 0;
+        Color32 newColor;
+        Color32 startColor = spriteRenderer.color;
+        Color32 endColor = new Color (255,255,255,255);
+
+        while( timeElapsed < agentSpeed)
+        {
+            newColor = Color.Lerp(startColor, endColor, timeElapsed/agentSpeed);
+            timeElapsed += Time.deltaTime;
+            spriteRenderer.color = newColor;
+            yield return null;
+        }
+        spriteRenderer.color = endColor;
+        animator.SetBool("isWalking", false);
+        agentColorCoroutine = null;
+        yield return null;
+    }
+    IEnumerator AgentPos()
+    {
+        float timeElapsed = 0;
+        float newPos;
+        float startPos = transform.position.y;
+        float endPos = 2.5f;
+
+        while(timeElapsed < agentSpeed)
+        {
+            newPos = Mathf.Lerp(startPos, endPos, timeElapsed/agentSpeed);
+            timeElapsed += Time.deltaTime;
+            transform.position = new Vector3(transform.position.x, newPos, transform.position.z);
+            yield return null;
+        }
+        transform.position = new Vector3(transform.position.x, endPos, transform.position.z);
+        agentPosCoroutine = null;
+        yield return new WaitForSeconds(2f);
+        typeWriterEffect.CallChangeText();
+        yield return null;
+    }
+}
---
+++ Assets/Scripts/TypeWriterEffect.cs
@@ -0,0 +1,128 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
+using System;
+// Typewriter effect source: https://gist.github.com/unitycoder/19625fed364a39cb278f
+
+// Handles text displayment on briefing monologues
+// Picks up parts of monologue from an array of TMP texts
+// Shows buttons to continue and start mission
+// If player hides the UI while text is playing, it will skip to the end
+public class TypeWriterEffect : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    public TextMeshProUGUI[] texts;
+    public bool playOnStart = true;
+    public float delayToStart = 0;
+    public float delayBetweenChars = 0.125f;
+    public float delayAfterPunctuation = 0.5f;
+    public float delayAfterSentence = 1f;
+    private string story;
+    private float originDelayBetweenChars;
+    private bool lastCharPunctuation = false;
+    private char charComma;
+    private char charPeriod;
+    private char charEmpty;
+    // Text colors
+    Color colVisible = new Color(255,255,255,255);
+    Color colHidden = new Color(255,255,255,0);
+    // Button
+    /*public GameObject continueButton;
+    public GameObject startButton;*/
+    // Text index
+    public int textIndex = 0;
+    // Misc
+    private bool firstTime = false; // Hack to prevent text from disappearing when player leaves interaction collider
+    void Awake() // Is run before OnEnable()
+    {
+        text = texts[0];
+        originDelayBetweenChars = delayBetweenChars;
+
+        charComma = Convert.ToChar(44);
+        charPeriod = Convert.ToChar(46);
+        charEmpty = Convert.ToChar(" ");//Convert.ToChar(255);
+    }
+    void Start()
+    {
+        if (playOnStart)
+        {
+            ChangeText(text.text, delayToStart);
+        }
+    }
+    /*void OnEnable() // If the parent object of this script is not unabled in the editor beofre starting, this will run. Better to use Start()
+    {
+        if (playOnStart)
+        {
+            ChangeText(text.text, delayToStart);
+        }
+    }*/
+    /*void OnDisable() // If player leaves the interaction collider, skip the text to the end
+    {
+        if(firstTime)
+        {
+            text.text = story;
+            if(textIndex != texts.Length - 1)
+                continueButton.SetActive(true);
+            else
+                startButton.SetActive(true);
+        }
+    }*/
+
+    //Update text and start typewriter effect
+    public void ChangeText(string textContent, float delayBetweenChars = 0f)
+    {
+        story = textContent;
+        text.text = ""; //clean text
+        Invoke("Start_PlayText", delayBetweenChars); //Invoke effect
+    }
+    public void Continue()
+    {
+        text.color = colHidden;
+        //continueButton.SetActive(false);
+        textIndex++; // each time we continue, the text index is incremented
+        text = texts[textIndex];
+        ChangeText(text.text, delayToStart);
+    }
+
+    public void Start_PlayText()
+    {
+        StartCoroutine(PlayText());
+    }
+
+    // This coroutine plays the text in "story" letter by letter
+    IEnumerator PlayText()
+    {
+        firstTime = true;
+        text.color = colVisible;
+        foreach (char c in story)
+        {
+            delayBetweenChars = originDelayBetweenChars;
+
+            if (lastCharPunctuation)  //If previous character was a comma/period, pause typing
+            {
+                yield return new WaitForSeconds(delayBetweenChars = delayAfterPunctuation);
+                lastCharPunctuation = false;
+            }
+
+            if ( c == charEmpty || c == charComma || c == charPeriod  )
+            {
+            lastCharPunctuation = true;
+            }
+
+            text.text += c;
+            yield return new WaitForSeconds(delayBetweenChars);
+        }
+        /*if(textIndex != texts.Length - 1)
+            continueButton.SetActive(true);
+        else
+            startButton.SetActive(true);*/
+        
+        yield return new WaitForSeconds(delayAfterSentence);
+    }
+    public void CallChangeText()
+    {
+        ChangeText(text.text, delayToStart);
+    }
+}
