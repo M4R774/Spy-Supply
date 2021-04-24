9d981f8fc9f4ea012e0d81ce170dd938d2b1dab7
tatu_heinamaki <tatu.heinamaki@gmail.com>
2021-04-24 16:16:54 +0300

    Added texts to item prefabs.

--- Assets/ScriptableObject/MissionData.cs
+++ Assets/ScriptableObject/MissionData.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "MissionData", menuName = "Data/MissionData", order = 1)]
@@ -21,5 +23,7 @@ public class MissionData : ScriptableObject
     public int poison_pill;
     public int salt_shaker;
     public int cross_country_skis;
-
+    public int shotgun;
+    public Dictionary<string, int> bazooka;
+    public List<KeyValuePair<string, int>> bazooka_1;
 }   
