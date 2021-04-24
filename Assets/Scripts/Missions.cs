517b02d079238b221d22ad4ff54ed92a21d38309
Martikainen Aleksi <aleksi.martikainen@nokia.com>
2021-04-24 15:27:37 +0300

    Add missions class

---
+++ Assets/Scripts/Missions.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missions : MonoBehaviour
+{
+    public List<Mission> missions;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Mission mission = new Mission();  // Test mission
+        missions.Add(mission);
+        
+        mission = new Mission(
+            "Title",
+            "description",
+            2,
+            "win_result",
+            "loss_result",
+            new Dictionary<string, int>
+            {
+                {"helmet", 1},
+                {"radio", -3}
+            }
+        );
+        missions.Add(mission);
+    }
+
+    public Mission GetRandomMission()
+    {
+        return missions[Random.Range(0, missions.Count - 1)];
+    }
+}
