35ca280f5f52d928739e9abd9679af5fbcd9d08c
tatu_heinamaki <tatu.heinamaki@gmail.com>
2021-04-24 12:08:17 +0300

    Created scritps for Report and CaseFile. Created functionality for opening report and case file with temp sprites.

---
+++ Assets/Scripts/CaseFile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaseFile : MonoBehaviour
+{
+    [SerializeField] private GameObject openedCaseFile;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    public void ShowCaseFile()
+    {
+        openedCaseFile.SetActive(true)
+;    }
+    public void HideCaseFile()
+    {
+        openedCaseFile.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Report.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Report : MonoBehaviour
+{
+    [SerializeField] private GameObject openedReport;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    public void ShowReport()
+    {
+        openedReport.SetActive(true)
+;    }
+    public void HideReport()
+    {
+        openedReport.SetActive(false);
+    }
+}
