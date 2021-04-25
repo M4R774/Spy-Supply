bb7413f838c30a2e2ccbf28223655d96d6095f1e
Tatu Lindroos <21022075+taturl@users.noreply.github.com>
2021-04-25 17:20:02 +0300

    Add broken case image

--- Assets/CaseFileController.cs
+++ Assets/CaseFileController.cs
@@ -2,27 +2,32 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CaseFileController : MonoBehaviour
 {
     public GameObject cameraObject;
     public GameObject header;
     public GameObject body;
+    public GameObject caseimage;
 
     public CameraController camera_controller;
     public TextMeshProUGUI header_text;
     public TextMeshProUGUI body_text;
+    public Image caseimage_image;
     
     void Start()
     {
         camera_controller = cameraObject.GetComponent<CameraController>(); 
         header_text = header.GetComponent<TextMeshProUGUI>();
         body_text = body.GetComponent<TextMeshProUGUI>();
+        caseimage_image = caseimage.GetComponent<Image>();
     }
 
     public void UpdateText()
     {
         body_text.text = camera_controller.current_mission.description;
         header_text.text = camera_controller.current_mission.title;
+        caseimage_image.sprite = Resources.Load<Sprite>(camera_controller.current_mission.picture_file_name);
     }
 }
